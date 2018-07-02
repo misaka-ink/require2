fe860814ad0b445d144f221148ad330c220585b0
SmilingXinyi <smilingxinyi@gmail.com>
2018-07-03 04:11:30 +0800

    improve singleton test

--- test/index.test.js
+++ test/index.test.js
@@ -6,10 +6,15 @@ const should = require('should')
 const require2 = require('../lib')
 
 describe('require2', function () {
-    it('should return demo module', function () {
-        const m1 = require2('@demo/module1')
-        const m2 = require2('@demo/module2')
-        m1.should.have.property('demo', true)
-        m2(m1).should.hava.property('demo', true)
+    const demoResult = {'demo': true}
+    const m1 = require2('@demo/module1')
+    const m2 = require2('@demo/module2')
+
+    it('require a object should equal to the target result', function () {
+        m1.should.deepEqual(demoResult)
+    })
+
+    it ('require a functon should return equal to the target result', function() {
+        m2(m1).should.deepEqual(demoResult)
     })
 })
